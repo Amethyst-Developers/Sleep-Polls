f6116725f8d28291706b401be819f254d59e25ab
Monishkrishna2009 <montagewala0@gmail.com>
2026-05-16 21:22:21 +0530

    Complete rewrite of SleepPolls

---
+++ app/build.gradle.kts
@@ -0,0 +1,181 @@
+import com.github.jengelman.gradle.plugins.shadow.tasks.ShadowJar
+import net.minecrell.pluginyml.paper.PaperPluginDescription
+import org.gradle.api.file.DuplicatesStrategy
+import org.gradle.jvm.toolchain.JavaLanguageVersion
+import org.gradle.jvm.tasks.Jar
+import org.gradle.api.tasks.compile.JavaCompile
+
+plugins {
+    java
+
+    id("io.papermc.paperweight.userdev") version "2.0.0-beta.21"
+    id("de.eldoria.plugin-yml.paper") version "0.9.0"
+    id("xyz.jpenilla.run-paper") version "3.0.2"
+    id("com.gradleup.shadow") version "9.2.2"
+}
+
+group = "org.amethystdev"
+version = "1.0.0"
+
+repositories {
+
+    mavenLocal()
+    mavenCentral()
+
+    maven("https://repo.papermc.io/repository/maven-public/")
+    maven("https://repo.codemc.io/repository/maven-releases/")
+    maven("https://repo.codemc.io/repository/maven-snapshots/")
+    maven("https://maven.enginehub.org/repo/")
+    maven("https://jitpack.io")
+    maven("https://repo.essentialsx.net/releases/")
+}
+
+dependencies {
+
+    // Paper
+    paperweight.paperDevBundle("26.1.2.build.+")
+    paperLibrary("com.google.code.gson:gson:2.13.1")
+
+    // Lamp command framework
+    val lampVersion = "4.0.0-rc.12"
+
+    implementation("io.github.revxrsal:lamp.common:$lampVersion")
+    implementation("io.github.revxrsal:lamp.bukkit:$lampVersion")
+    compileOnly("net.essentialsx:EssentialsX:2.21.1") {
+        isTransitive = false
+    }
+}
+
+java {
+
+    toolchain {
+        languageVersion.set(JavaLanguageVersion.of(25))
+    }
+
+    withSourcesJar()
+    withJavadocJar()
+}
+
+tasks {
+
+    withType<JavaCompile>().configureEach {
+
+        options.release.set(25)
+        options.encoding = "UTF-8"
+
+        // Preserve parameter names
+        options.compilerArgs.add("-parameters")
+    }
+
+    withType<Jar>().configureEach {
+        duplicatesStrategy = DuplicatesStrategy.EXCLUDE
+    }
+
+    processResources {
+        filteringCharset = "UTF-8"
+    }
+
+    build {
+        dependsOn(shadowJar)
+    }
+
+    named<ShadowJar>("shadowJar") {
+
+        archiveBaseName.set("SleepPolls")
+        archiveClassifier.set("")
+
+        relocate(
+            "io.github.revxrsal",
+            "${project.group}.libs.lamp"
+        )
+
+        mergeServiceFiles()
+    }
+
+    generatePaperPluginDescription {
+        useDefaultCentralProxy()
+    }
+
+    runServer {
+
+        minecraftVersion("26.1.2")
+
+        jvmArgs(
+            "-Xms2G",
+            "-Xmx2G"
+        )
+
+        downloadPlugins {
+
+            // ServiceIO
+            hangar("ServiceIO", "2.6.0")
+
+            // WorldEdit
+            modrinth("worldedit", "k9KdTr1M")
+
+            // LuckPerms
+            url("https://download.luckperms.net/1631/bukkit/loader/LuckPerms-Bukkit-5.5.42.jar")
+
+            // MiniPlaceholders
+            modrinth("miniplaceholders", "N2WfJ0ll")
+
+            // ViaVersion
+            modrinth("viaversion", "BqLcExWb")
+
+            // ViaBackwards
+            modrinth("viabackwards", "whyGF8dZ")
+        }
+    }
+}
+
+paper {
+
+    name = "SleepPolls"
+
+    version = project.version.toString()
+
+    description = "A modern sleep voting system for Paper servers that transforms night-time gameplay with democratic polling."
+
+    main = "org.amethystdev.Main"
+
+    apiVersion = "26.1"
+
+    prefix = "SleepPolls"
+
+    authors = listOf(
+        "Monk",
+        "NichuNaizam",
+        "The Amethyst Team"
+    )
+
+    bootstrapper = "org.amethystdev.Bootstrapper"
+    loader = "org.amethystdev.PluginLibrariesLoader"
+
+    hasOpenClassloader = false
+    generateLibrariesJson = true
+
+    foliaSupported = true
+
+    serverDependencies {
+
+        register("LuckPerms") {
+            required = false
+            load = PaperPluginDescription.RelativeLoadOrder.BEFORE
+        }
+
+        register("ViaVersion") {
+            required = false
+            load = PaperPluginDescription.RelativeLoadOrder.BEFORE
+        }
+
+        register("WorldEdit") {
+            required = false
+            load = PaperPluginDescription.RelativeLoadOrder.BEFORE
+        }
+
+        register("MiniPlaceholders") {
+            required = false
+            load = PaperPluginDescription.RelativeLoadOrder.BEFORE
+        }
+    }
+}
---
+++ app/src/main/java/org/amethystdev/sleep/SleepListener.java
@@ -0,0 +1,207 @@
+/*
+ * This file is part of Sleep-Polls - https://github.com/Amethyst-Developers/Sleep-Polls
+ * Copyright (C) 2026  Monk (Monish), The Amethyst Team and contributors
+ * 
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ * 
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ * 
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <https://www.gnu.org/licenses/>.
+ */
+
+package org.amethystdev.sleep;
+
+import java.util.Set;
+import java.util.stream.Collectors;
+
+import org.amethystdev.Main;
+import org.bukkit.Bukkit;
+import org.bukkit.GameMode;
+import org.bukkit.World;
+import org.bukkit.entity.Player;
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.Listener;
+import org.bukkit.event.player.PlayerBedEnterEvent;
+
+import net.ess3.api.IEssentials;
+import net.kyori.adventure.text.Component;
+import net.kyori.adventure.text.format.NamedTextColor;
+import net.kyori.adventure.text.format.Style;
+import net.kyori.adventure.text.format.TextDecoration;
+
+public final class SleepListener implements Listener {
+
+    private static final Component PREFIX =
+            Component.text(
+                    "[SleepPoll] ",
+                    Style.style(
+                            NamedTextColor.GOLD,
+                            TextDecoration.BOLD
+                    )
+            );
+
+    private final Main plugin;
+
+    private final SleepPollManager manager;
+
+    public SleepListener(
+            Main plugin,
+            SleepPollManager manager
+    ) {
+
+        this.plugin = plugin;
+        this.manager = manager;
+    }
+
+    @EventHandler
+    public void onPlayerBedEnter(PlayerBedEnterEvent event) {
+
+        // Bed entry failed
+        if (event.getBedEnterResult()
+                != PlayerBedEnterEvent.BedEnterResult.OK) {
+            return;
+        }
+
+        Player sleeper = event.getPlayer();
+
+        World world = sleeper.getWorld();
+
+        String worldName = world.getName();
+
+        // Ignore blacklisted worlds
+        if (plugin.isWorldBlocked(worldName)) {
+            return;
+        }
+
+        long time = world.getTime();
+
+        // Only allow at night
+        if (time < 13000 || time > 23000) {
+
+            sleeper.sendMessage(
+                    PREFIX.append(
+                            Component.text(
+                                    "Sleep polls are only available during night time!",
+                                    NamedTextColor.RED
+                            )
+                    )
+            );
+
+            return;
+        }
+
+        // Prevent duplicate polls
+        if (manager.isPollActive(worldName)) {
+
+            event.setCancelled(true);
+
+            sleeper.sendMessage(
+                    PREFIX.append(
+                            Component.text(
+                                    "A poll is already active! ",
+                                    NamedTextColor.YELLOW
+                            )
+                    ).append(
+                            Component.text(
+                                    "Please vote with ",
+                                    NamedTextColor.GRAY
+                            )
+                    ).append(
+                            Component.text(
+                                    "/sleeppoll yes ",
+                                    NamedTextColor.GREEN
+                            )
+                    ).append(
+                            Component.text(
+                                    "or ",
+                                    NamedTextColor.GRAY
+                            )
+                    ).append(
+                            Component.text(
+                                    "/sleeppoll no",
+                                    NamedTextColor.RED
+                            )
+                    ).append(
+                            Component.text(
+                                    ".",
+                                    NamedTextColor.GRAY
+                            )
+                    )
+            );
+
+            return;
+        }
+
+        Set<Player> voters = world.getPlayers()
+                .stream()
+
+                // Online only
+                .filter(Player::isOnline)
+
+                // Ignore AFK players
+                .filter(p -> !isAfk(p))
+
+                // Ignore spectators
+                .filter(p ->
+                        p.getGameMode()
+                                != GameMode.SPECTATOR
+                )
+
+                // Ignore dead players
+                .filter(p -> !p.isDead())
+
+                .collect(Collectors.toSet());
+
+        // Minimum players check
+        if (voters.size() < 2) {
+
+            sleeper.sendMessage(
+                    PREFIX.append(
+                            Component.text(
+                                    "Not enough active players online to start a sleep poll.",
+                                    NamedTextColor.RED
+                            )
+                    )
+            );
+
+            return;
+        }
+
+        event.setCancelled(true);
+
+        manager.startPoll(
+                worldName,
+                voters
+        );
+    }
+
+    private boolean isAfk(Player player) {
+
+        if (Bukkit.getPluginManager()
+                .getPlugin("Essentials") == null) {
+            return false;
+        }
+
+        try {
+
+            IEssentials essentials =
+                    (IEssentials) Bukkit.getPluginManager()
+                            .getPlugin("Essentials");
+
+            return essentials
+                    .getUser(player)
+                    .isAfk();
+
+        } catch (Exception ignored) {
+
+            return false;
+        }
+    }
+}
